905d30a8dc28ba5933ed5782502650b8ef081037
larmijo-oss <larmijo@umich.edu>
2026-09-06 16:01:33 -0400

    Changed git ignore, first commit

---
+++ Agent/Agent/AgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgentApp.swift
+//  Agent
+//
+//  Created by Lena Armijo on 9/4/26.
+//
+
+import SwiftUI
+
+@main
+struct AgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Agent/Agent/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Agent
+//
+//  Created by Lena Armijo on 9/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
